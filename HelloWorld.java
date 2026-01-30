6bf2af780061b941e106f167fc2afe20d8686be6
Vincent Cheng <chengv3@rpi.edu>
2026-01-29 19:58:56 -0500

    Adding new print line\n

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!\n");
+        System.out.println("Hi, this is form user 2.\n");
     }
 }
